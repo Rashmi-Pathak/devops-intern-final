dfc7de2f5559e2aa5aa7091086fb5eb7aa12daf3
Rashmi-Pathak <rashmipathak2006@gmail.com>
2025-12-03 12:49:34 +0530

    Step 2: Added executable sysinfo.sh script

---
+++ scripts/sysinfo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# System Information Script
+
+echo "--- Current User ---"
+whoami # Prints the current user [cite: 18]
+
+echo -e "\n--- Current Date ---"
+date # Prints the current date [cite: 19]
+
+echo -e "\n--- Disk Usage (df -h) ---"
+df -h # Prints disk usage in human-readable format [cite: 20]
